2ab7051fa5e19a1590d053f26d44cf74c4be2856
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-03-25 13:54:35 -0500

    added further validation for name and age

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 
 function createGreeting (name, age){
   if (!name || !age) throw new Error('Arguments not valid');
+  if (typeof name !== 'string') throw new Error('Name must be a string');
+  if (isNaN(age)) throw new Error('Age needs to be a number');
   if (age < 0) throw new TypeError('Age cannot be negative');
+
   return `My name is ${name} and I am ${age} years old. I was born in ${getYearOfBirth(age)}`;
 }
 
 function getYearOfBirth(age){
-  return (2019 - Number(age));
+  return (2019 - age);
 }
 
 try {
@@ -18,3 +21,4 @@ try {
 }
 
 
+// What happens if you pass "29" (string) as the age? What about "twenty nine"? Implement a check to ensure the name is a string and the age is a number. If not, throw new TypeError().
